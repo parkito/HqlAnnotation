6baa1c7eff5f5207f1deb697b35d8ecd47789861
parkito <artyom-karnov@yandex.ru>
2018-04-26 20:54:17 +0300

    Change context policy

---
+++ src/main/java/ru/siksmfp/spring/hibernatehsql/annotation/ContextStartedEventListener.java
@@ -0,0 +1,8 @@
+package ru.siksmfp.spring.hibernatehsql.annotation;
+
+/**
+ * @author Artem Karnov @date 4/26/2018.
+ * @email artem.karnov@t-systems.com
+ */
+public class ContextStartedEventListner {
+}
